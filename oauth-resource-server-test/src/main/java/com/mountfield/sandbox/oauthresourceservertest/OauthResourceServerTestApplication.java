02a51af004c34fbf5de1e79f5c029a62ac4e7782
Alfred Mountfield <alfred.mountfield@student.manchester.ac.uk>
2020-03-13 17:32:49 +0000

    Initial commit

---
+++ oauth-resource-server-test/src/main/java/com/mountfield/sandbox/oauthresourceservertest/OauthResourceServerTestApplication.java
@@ -0,0 +1,13 @@
+package com.mountfield.sandbox.oauthresourceservertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthResourceServerTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthResourceServerTestApplication.class, args);
+	}
+
+}
