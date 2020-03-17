893206ea94e7b8ce222da29a229eccc57f3232f0
Alfred <alfredmountfield@gmail.com>
2020-03-17 11:39:28 +0000

    Migrating to gradle

---
+++ oauth-resource-server/src/main/java/com/mountfield/sandbox/oauth/resource/server/OauthResourceServerTestApplication.java
@@ -0,0 +1,13 @@
+package com.mountfield.sandbox.oauth.resource.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthResourceServerTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthResourceServerTestApplication.class, args);
+	}
+
+}
---
+++ oauth-resource-server/src/test/java/com/mountfield/sandbox/oauth/resource/server/OauthResourceServerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mountfield.sandbox.oauth.resource.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthResourceServerTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
